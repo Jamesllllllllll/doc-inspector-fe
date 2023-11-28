d2880e2dcb3d59e5a2c4bc371c81f40e6a291c37
James <jkeezer@pm.me>
2023-11-28 16:09:02 -0500

    Refactor PDFdownload

--- app/components/PDFdownload.js
+++ app/components/PDFdownload.js
@@ -14,98 +14,6 @@ import {
 import { Button } from '@mui/material';
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
 
-const markdownToJSX = (markdown) => {
-  const lines = markdown.split('\n');
-  let jsxContent = [];
-
-  lines.forEach((line, index) => {
-    if (line.startsWith('# ')) {
-      jsxContent.push(
-        <Text key={index} style={styles.h1}>
-          {line.substring(2)}
-        </Text>
-      );
-    } else if (line.startsWith('## ')) {
-      jsxContent.push(
-        <Text key={index} style={styles.h2}>
-          {line.substring(3)}
-        </Text>
-      );
-    } else if (line.trim() === '') {
-      jsxContent.push(
-        <Text key={index} style={styles.newLine}>
-          {' '}
-        </Text>
-      );
-    } else {
-      let segments = line.split(/(\*\*.*?\*\*|\*.*?\*)/g);
-      let formattedLine = segments.map((segment, segmentIndex) => {
-        if (segment.startsWith('**')) {
-          return (
-            <Text key={segmentIndex} style={styles.bold}>
-              {segment.slice(2, -2)}
-            </Text>
-          );
-        } else if (segment.startsWith('*')) {
-          return (
-            <Text key={segmentIndex} style={styles.italic}>
-              {segment.slice(1, -1)}
-            </Text>
-          );
-        } else {
-          return segment;
-        }
-      });
-      jsxContent.push(
-        <Text key={index} style={styles.p}>
-          {formattedLine}
-        </Text>
-      );
-    }
-  });
-  return jsxContent;
-};
-
-export default function PDFdownload() {
-  const { file } = useContext(AppContext);
-  const markdownString = file.template;
-  const jsxContent = markdownToJSX(markdownString);
-
-  const MyDocument = () => (
-    <Document>
-      <Page size='A4' style={styles.page}>
-        <View style={styles.section}>{jsxContent}</View>
-      </Page>
-    </Document>
-  );
-
-  const filename = file?.pdf?.name?.split('.')[0] || '';
-
-  return (
-    <>
-      <div className='flex flex-row justify-end items-center gap-4'>
-        <p className='text-gray-500'>Download your generated document:</p>
-        <PDFDownloadLink
-          document={<MyDocument />}
-          fileName={`DocInspector_${filename}.pdf`}
-          className='self-end'
-        >
-          {({ blob, url, loading, error }) =>
-            loading ? (
-              'Loading document...'
-            ) : (
-              <Button variant='outlined' startIcon={<CloudDownloadIcon />}>
-                Download PDF
-              </Button>
-            )
-          }
-        </PDFDownloadLink>
-      </div>
-      <MyDocument />
-    </>
-  );
-};
-
 Font.register({
   family: 'var(--font-oswald)',
   src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf',
@@ -163,3 +71,107 @@ const styles = StyleSheet.create({
   },
 });
 
+const markdownMap = {
+  '#### ': { style: styles.h4, substring: 5 },
+  '### ': { style: styles.h3, substring: 4 },
+  '## ': { style: styles.h2, substring: 3 },
+  '# ': { style: styles.h1, substring: 2 },
+};
+
+const markdownToJSX = (markdown) => {
+  const lines = markdown.split('\n');
+  let jsxContent = [];
+
+  lines.forEach((line, index) => {
+    let handled = false;
+
+    for (const key in markdownMap) {
+      if (line.startsWith(key)) {
+        const { style, substring } = markdownMap[key];
+        jsxContent.push(
+          <Text key={index} style={style}>
+            {line.substring(substring)}
+          </Text>
+        );
+        handled = true;
+        break;
+      }
+    }
+
+    if (!handled) {
+      if (line.trim() === '') {
+        jsxContent.push(
+          <Text key={index} style={styles.newLine}>
+            {' '}
+          </Text>
+        );
+      } else {
+        let segments = line.split(/(\*\*.*?\*\*|\*.*?\*)/g);
+        let formattedLine = segments.map((segment, segmentIndex) => {
+          if (segment.startsWith('**')) {
+            return (
+              <Text key={segmentIndex} style={styles.bold}>
+                {segment.slice(2, -2)}
+              </Text>
+            );
+          } else if (segment.startsWith('*')) {
+            return (
+              <Text key={segmentIndex} style={styles.italic}>
+                {segment.slice(1, -1)}
+              </Text>
+            );
+          } else {
+            return segment;
+          }
+        });
+        jsxContent.push(
+          <Text key={index} style={styles.p}>
+            {formattedLine}
+          </Text>
+        );
+      }
+    }
+  });
+
+  return jsxContent;
+};
+
+export default function PDFdownload() {
+  const { file } = useContext(AppContext);
+  const markdownString = file.template;
+  const jsxContent = markdownToJSX(markdownString);
+
+  const MyDocument = () => (
+    <Document>
+      <Page size='A4' style={styles.page}>
+        <View style={styles.section}>{jsxContent}</View>
+      </Page>
+    </Document>
+  );
+
+  const filename = file?.pdf?.name?.split('.')[0] || '';
+
+  return (
+    <>
+      <div className='flex flex-row justify-end items-center gap-4'>
+        <p className='text-gray-500'>Download your generated document:</p>
+        <PDFDownloadLink
+          document={<MyDocument />}
+          fileName={`DocInspector_${filename}.pdf`}
+          className='self-end'
+        >
+          {({ blob, url, loading, error }) =>
+            loading ? (
+              'Loading document...'
+            ) : (
+              <Button variant='outlined' startIcon={<CloudDownloadIcon />}>
+                Download PDF
+              </Button>
+            )
+          }
+        </PDFDownloadLink>
+      </div>
+      <MyDocument />
+    </>
+  );
+};
